09731830cf3ed17908ea3f5216326dfc8c88cea7
jesse-jacks0n <amamjesse@gmail.com>
2024-11-26 13:24:40 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,7 @@ import { getDatabase } from "firebase/database";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCbwvgf7nEcqVvZtnNU_Ejq1ckBanh_9d4",
-    authDomain: "fir-97177.firebaseapp.com",
-    databaseURL: "https://fir-97177-default-rtdb.firebaseio.com",
-    projectId: "fir-97177",
-    storageBucket: "fir-97177.appspot.com",
-    messagingSenderId: "93004975384",
-    appId: "1:93004975384:web:a0e1f0761771fcb253226d",
-    measurementId: "G-4GRGDFEKWF"
+   
 };
 
 // Initialize Firebase
